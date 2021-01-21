388205c01496df158c1e3b7158b60024f245f43e
Dmitriy Rovenskiy <arhitec@bk.ru>
2021-01-21 22:45:26 +0300

    code v5.0

--- server.ws.complete.js
+++ server.ws.complete.js
@@ -51,57 +51,51 @@ const port = process.env.PORT || 7070;
 const server = http.createServer(app.callback())
 const wsServer = new WS.Server({ server });
 
-wsServer.on('connection', (ws, req) => {
+wsServer.on('connection', (ws, req) => { //подписываемся на событие конэкшн
   sockets.add(ws);
   ws.on('message', data => {
-    const request = JSON.parse(data);
-    console.log(request);
-    if (request.type === 'add user' && users.has(request.nickname)) {
+    const request = JSON.parse(data); //парсим запрос
+    console.log(request); //чтение запроса
+    console.log(wsServer.clients.size); //информация о кол-ве подключённых пользователей
+    if (request.type === 'add user' && users.has(request.nickname)) { //проверяем огригинальность имени
       const msg = {type: 'error', text: 'User with the same has already exist'};
-      const response = JSON.stringify(msg);
+      const response = JSON.stringify(msg); //если имя существует
       ws.send(response);
-    } else if (request.type === 'add user') {
+    } else if (request.type === 'add user') { //если имя оригинально
       users.add(request.nickname);
       const msg = {type: 'user added', users: [...users]};
-      const response = JSON.stringify(msg);
-      console.log('---new user---');
-      console.log(response);
-      console.log('---new user---');
-
+      const response = JSON.stringify(msg); //создаём ответ
+      console.log(wsServer.clients.size); //информация о кол-ве подключённых пользователей
       const nicknameValid = {type: 'nickname is valid', nickname: request.nickname};
       const responseNickname = JSON.stringify(nicknameValid);
-      ws.send(responseNickname);
-
-      sendResponse(response);
-    } else if (request.type === 'get users') {
+      ws.send(responseNickname); //отправляем юзеру выславшему запрос положитенльный ответ
+      sendResponse(response); //отправляем всем юзерам новый список
+    } else if (request.type === 'get users') { //отправляем список юзеров на запрос при загрузке страници
       const msg = { type: 'users list', users: [...users]}
       const response = JSON.stringify(msg);
       ws.send(response);
-    } else if (request.type === 'message') {
+    } else if (request.type === 'message') { //отправляем сообщения
       console.log(request.nickname);
       const msg = {type: 'message', text: request.text, name: request.nickname };
       const response = JSON.stringify(msg);
-      console.log('---message text---');
-      console.log(response);
-      console.log('---message text---')
-      sendResponse(response)
+      sendResponse(response); //направляем сообщение всем пользователям
+    } else if (request.type === 'delete') { //юзер покинул чат
+      users.delete(request.nickname); //удаляем пользователя
+      console.log(users);
+      const msg = { type: 'users list', users: [...users]}
+      const response = JSON.stringify(msg);
+      sendResponse(response);
+      console.log('user delete');
     }
-
-
-
-    
-
-    /*
-    [...wsServer.clients]
-    .filter(channel => channel.readyState === WS.OPEN)
-    .forEach(channel => {channel.send('some message')});
-    */
   });
 
-  ws.send('welcome');
+  ws.on('close', (event) => { //удаляем клиента при ws.close
+    wsServer.clients.delete(ws);
+    console.log(wsServer.clients.size)
+  })
 });
 
-function sendResponse(response) {
+function sendResponse(response) { //функция для отправки сообщения всем пользователям
   [...wsServer.clients]
     .filter(channel => channel.readyState === WS.OPEN)
     .forEach(channel => {channel.send(response)});
